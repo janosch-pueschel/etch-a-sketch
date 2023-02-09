fb95e21d47a22e35120f302a92fa299dbe43b236
Janosch Püschel <janosch.pueschel@gmail.com>
2023-02-09 23:28:50 +0100

    bug fixed

--- script.js
+++ script.js
@@ -2,15 +2,9 @@
 
 const gridContainer = document.querySelector(".grid-container");
 const body = document.querySelector("body");
-const newGridBtn = document.createElement("button");
-let numberOfSquares;
 
+const newGridBtn = document.createElement("button");
 newGridBtn.textContent = "New Grid";
-newGridBtn.addEventListener("click", () => {
-  newNumberOfSquares();
-  console.log(numberOfSquares);
-  newGrid();
-});
 body.appendChild(newGridBtn);
 
 for (let i = 0; i < 16 * 16; i++) {
@@ -22,6 +16,7 @@ for (let i = 0; i < 16 * 16; i++) {
   gridContainer.appendChild(colorContainer);
 }
 
+let numberOfSquares;
 function newNumberOfSquares() {
   numberOfSquares = 0;
   numberOfSquares = prompt(
@@ -29,14 +24,10 @@ function newNumberOfSquares() {
   );
   if (numberOfSquares > 100) {
     alert("Maximum number of squares is 100.");
-    numberOfSquares = prompt(
-      "How many squares do you want your new grid to have?"
-    );
+    newNumberOfSquares();
   } else if (numberOfSquares < 1) {
     alert("Your new grid has to have at least 1 square.");
-    numberOfSquares = prompt(
-      "How many squares do you want your new grid to have?"
-    );
+    newNumberOfSquares();
   }
 }
 
@@ -45,10 +36,15 @@ function newGrid() {
   for (let i = 0; i < numberOfSquares * numberOfSquares; i++) {
     const colorContainer = document.createElement("div");
     colorContainer.classList.add("color-container");
-    colorContainer.addEventListener("mouseover", () => {
+    colorContainer.addEventListener("click", () => {
       colorContainer.classList.add("color-container--hover");
     });
     gridContainer.style.cssText = `grid-template: repeat(${numberOfSquares}, 1fr) / repeat(${numberOfSquares}, 1fr);`;
     gridContainer.appendChild(colorContainer);
   }
 }
+
+newGridBtn.addEventListener("click", () => {
+  newNumberOfSquares();
+  newGrid();
+});
