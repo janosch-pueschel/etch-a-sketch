7745a5aa1c05596b48ecb91d98deaba17529a343
Janosch Püschel <janosch.pueschel@gmail.com>
2023-02-20 11:08:52 +0100

    added function to change div color at mousedown

--- script.js
+++ script.js
@@ -16,6 +16,11 @@ function renderGrid() {
     const colorContainer = document.createElement("div");
     colorContainer.classList.add("color-container");
     colorContainer.addEventListener("mouseover", changeColor);
+    colorContainer.addEventListener("mousedown", function () {
+      getColor();
+      colorContainer.style.backgroundColor = color;
+      console.log("click")
+    });
     gridContainer.appendChild(colorContainer);
   }
 }
